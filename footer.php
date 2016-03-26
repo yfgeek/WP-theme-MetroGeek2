f45c0b0c39500fb19ac476d0c97a48c916077a21
ivan <i@yfgeek.com>
2016-03-26 18:12:01 +0800

    升级2.1
    细节优化，界面美化，评论区优化，点击头像回复内容

--- footer.php
+++ footer.php
@@ -1,1 +1,91 @@
-</div><div id="footer"><div class="man">	<div id="links">	<div class="lk">L<br/>I<br/>N<br/>K</div>	<ul class="u1">	<li><a href="http://luoyandi.com">幽虫</a></li>	<li><a href="http://hexiaz.com">傅小黑</a></li>	<li><a href="http://safe121.com">网络安全技术</a></li>	<li><a href="http://dresita.info/">Dresita</a></li>	</ul>	<ul class="u2">	<li><a href="http://sankoshine.lofter.com/">SanKo</a></li>	<li><a href="http://www.echowxsy.com/">Echowxsy</a></li>	<li><a href="http://www.xiexiaolan.com">XieXiaoLan</a></li>	<li><a href="">暂无友情链接</a></li>	</ul>		<ul class="u3">	<li><a href="">暂无友情链接</a></li>	<li><a href="">暂无友情链接</a></li>	<li><a href="">暂无友情链接</a></li>	<li><a href="http://51.la/?15156642">统计</a></li>	</ul>			</div>	<div class="ri"><br /><br /><br /><br />	<p>yifanb(at)gmail.com</p>	<p>Load time:<?php timer_stop(1); ?>s</p>		<p>Server is running in Hong Kong</p>	<p><b>Geek Metro V2.0 IE11/Chrome</b></p>	<p><b>Powered by WordPress.Copyleft theme by YiFan </b></p>	</div></div></div><div style="display:none"><script language="javascript" type="text/javascript" src="http://js.users.51.la/15156642.js"></script></div></body></HTML>
+</div>
+<div id="footer">
+  <div class="man">
+    <div id="links">
+      <div class="lk">
+        L
+        <br/>
+        I
+        <br/>
+        N
+        <br/>
+        K
+      </div>
+      <ul class="u1">
+        <li>
+          <a href="http://luoyandi.com">
+            幽虫
+          </a>
+        </li>
+        <li>
+          <a href="http://hexiaz.com">
+            傅小黑
+          </a>
+        </li>
+        <li>
+          <a href="http://safe121.com">
+            网络安全技术
+          </a>
+        </li>
+        <li>
+          <a href="http://dresita.info/">
+            Dresita
+          </a>
+        </li>
+      </ul>
+      <ul class="u2">
+        <li>
+          <a href="http://sankoshine.lofter.com/">
+            SanKo
+          </a>
+        </li>
+        <li>
+          <a href="http://www.echowxsy.com/">
+            Echowxsy
+          </a>
+        </li>
+        <li>
+          <a href="http://www.xiexiaolan.com">
+            XieXiaoLan
+          </a>
+        </li>
+        <li>
+          <a href="">
+            暂无链接
+          </a>
+        </li>
+      </ul>
+      <ul class="u3">
+        <li>
+          <a href="">
+            暂无友情链接
+          </a>
+        </li>
+        <li>
+          <a href="">
+            暂无链接
+          </a>
+        </li>
+        <li>
+          <a href="">
+            暂无链接
+          </a>
+        </li>
+        <li>
+          <a href="http://51.la/?15156642">
+            统计
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="ri">
+    </div>
+  </div>
+</div>
+<div style="display:none">
+  <script language="javascript" type="text/javascript" src="http://js.users.51.la/15156642.js">
+  </script>
+</div>
+</body>
+
+</HTML>
